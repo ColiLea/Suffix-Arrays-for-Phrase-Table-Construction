9b598bfb8be5a1236294de1939e5fda1e0679f3f
Lea Frermann <l1@frermann.de>
2011-07-17 12:25:24 +0200

    working SuffixArray Creation

--- suffixarrays.go
+++ suffixarrays.go
@@ -2,64 +2,66 @@ package main
 
 import "fmt"
 import "strings"
-import "io/ioutil"
 import "sort"
 // import "regexp"
 // import "math"
 
 type SuffixArray struct{
-	i []int
-	s []string
+        i []int
+        s []string
 }
 
-func NewSuffixArray(str string) (suffixArray *SuffixArray) {
-	suffixArray = new(SuffixArray)
-	suffixArray.s = strings.Split(str, " ", -1)
-	suffixArray.i = make([]int, len(suffixArray.s))
-	for idx, _ := range suffixArray.s {
-		suffixArray.i[idx] = idx
-	}
-	sort.Sort(suffixArray)
-	return
+func NewSuffixArray(str ... string) (suffixArray *SuffixArray) {
+        suffixArray = new(SuffixArray)
+        suffixArray.s=str
+        suffixArray.i = make([]int, len(str))
+        for idx := range suffixArray.i {
+                suffixArray.i[idx] = idx
+        }
+        sort.Sort(suffixArray)
+        return
 }
 
 func (suffixArray *SuffixArray) Len() (int) {
-	return len(suffixArray.i)
+        return len(suffixArray.i)
 }
 
-func (suffixArray *SuffixArray) Less(m, n int) (b bool) {
-	switch {
-		case suffixArray.s[suffixArray.i[m]] < suffixArray.s[suffixArray.i[n]] :
-			b = true
-			fmt.Println(1)
-		case suffixArray.s[suffixArray.i[m]] > suffixArray.s[suffixArray.i[n]] :
-			b = false
-			fmt.Println(2)
-		case suffixArray.s[suffixArray.i[m]] == suffixArray.s[suffixArray.i[n]] :
-			if n == len(suffixArray.i)-1 {
-				b = false
-				fmt.Println(3)
-			} else if m == len(suffixArray.i)-1 {
-				b = true
-				fmt.Println(4)
-			} else {
-				b = suffixArray.Less(m+1, n+1)
-				fmt.Println(5)
-			}
-	}
-	return b
+func (suffixArray *SuffixArray) Less(m, n int) (bool) {
+        midx:=suffixArray.i[m]
+        nidx:=suffixArray.i[n]
+        for{
+                switch {
+                        case suffixArray.s[midx]<suffixArray.s[nidx] :
+                                return true
+                        case suffixArray.s[midx]>suffixArray.s[nidx] :
+                                return false
+                        case suffixArray.s[midx]==suffixArray.s[nidx] :
+                                midx++
+                                nidx++
+                                if nidx == len(suffixArray.i) {
+                                        return false
+                                } else if midx == len(suffixArray.i) {
+                                        return true
+                                }
+                }
+        }
+        panic("unreachable")
 }
 
 
 func (suffixArray *SuffixArray) Swap(m, n int) {
-	suffixArray.i[m], suffixArray.i[n] = suffixArray.i[n], suffixArray.i[m]
+        suffixArray.i[m], suffixArray.i[n] = suffixArray.i[n], suffixArray.i[m]
+}
+
+func (suffixArray *SuffixArray) String() (string){
+        array := make([]string, len(suffixArray.i))
+        for idx, element := range suffixArray.i {
+                array[idx] = strings.Join(suffixArray.s[element:], " ")
+        }
+        return strings.Join(array, "\n")
 }
 
 func main() {
-	sourceFile,_ := ioutil.ReadFile("./corpora/test")
-	sentences := strings.Split(string(sourceFile), "\n", -1)
-	for _, element := range sentences {
-		suffixArray := NewSuffixArray(element)
-		fmt.Println(suffixArray)
-	}
+                suffixArray := NewSuffixArray("a","b","r","a","c","a","d","a","b","r","a")
+                fmt.Println(suffixArray)
 }
