bbb7825af33f31b58a25e1152d3e68937bf0335a
Lea Frermann <l1@frermann.de>
2011-07-18 15:24:45 +0200

    split code in libraries (-> textprocessing)

--- suffixarrays.go
+++ suffixarrays.go
@@ -1,67 +1,18 @@
 package main
 
 import "fmt"
-import "strings"
-import "sort"
-// import "regexp"
-// import "math"
+import "textprocessing"
 
-type SuffixArray struct{
-        i []int
-        s []string
-}
-
-func NewSuffixArray(str ... string) (suffixArray *SuffixArray) {
-        suffixArray = new(SuffixArray)
-        suffixArray.s=str
-        suffixArray.i = make([]int, len(str))
-        for idx := range suffixArray.i {
-                suffixArray.i[idx] = idx
-        }
-        sort.Sort(suffixArray)
-        return
-}
-
-func (suffixArray *SuffixArray) Len() (int) {
-        return len(suffixArray.i)
-}
-
-func (suffixArray *SuffixArray) Less(m, n int) (bool) {
-        midx:=suffixArray.i[m]
-        nidx:=suffixArray.i[n]
-        for{
-                switch {
-                        case suffixArray.s[midx]<suffixArray.s[nidx] :
-                                return true
-                        case suffixArray.s[midx]>suffixArray.s[nidx] :
-                                return false
-                        case suffixArray.s[midx]==suffixArray.s[nidx] :
-                                midx++
-                                nidx++
-                                if nidx == len(suffixArray.i) {
-                                        return false
-                                } else if midx == len(suffixArray.i) {
-                                        return true
-                                }
-                }
-        }
-        panic("unreachable")
-}
-
-
-func (suffixArray *SuffixArray) Swap(m, n int) {
-        suffixArray.i[m], suffixArray.i[n] = suffixArray.i[n], suffixArray.i[m]
-}
-
-func (suffixArray *SuffixArray) String() (string){
-        array := make([]string, len(suffixArray.i))
-        for idx, element := range suffixArray.i {
-                array[idx] = strings.Join(suffixArray.s[element:], " ")
-        }
-        return strings.Join(array, "\n")
-}
 
 func main() {
-                suffixArray := NewSuffixArray("a","b","r","a","c","a","d","a","b","r","a")
-                fmt.Println(suffixArray)
+// 	sourceFile,_ := ioutil.ReadFile("../corpora/test")
+// 	sentences := strings.Split(string(sourceFile), "\n", -1)
+// 	for _, element := range sentences {
+// 		words := strings.Split(element, " ", -1)
+// 		suffixArray := NewSuffixArray(words)
+// 		fmt.Println(suffixArray)
+// 	}
+	suffixArray := textprocessing.NewSuffixArray("a","b","r","a","c","a","d","a","b","r","a")
+	fmt.Println(suffixArray)
+	fmt.Println(suffixArray.SuffixOfLength(4))
 }
