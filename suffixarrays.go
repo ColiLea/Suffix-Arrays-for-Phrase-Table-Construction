49555d873da0ac3780ed9ece6e6d55ab64f98274
Lea Frermann <l1@frermann.de>
2011-07-28 19:38:07 +0200

    Change in String() and SuffixString(); SuffixString(n) returns suffix of length n

--- suffixarrays.go
+++ suffixarrays.go
@@ -5,13 +5,6 @@ import "textprocessing"
 
 
 func main() {
-// 	sourceFile,_ := ioutil.ReadFile("../corpora/test")
-// 	sentences := strings.Split(string(sourceFile), "\n", -1)
-// 	for _, element := range sentences {
-// 		words := strings.Split(element, " ", -1)
-// 		suffixArray := NewSuffixArray(words)
-// 		fmt.Println(suffixArray)
-// 	}
 	input := textprocessing.NewText("a b r a c a d a b r a")
 	suffixArray := textprocessing.NewSuffixArray(input)
 	fmt.Println(suffixArray)
