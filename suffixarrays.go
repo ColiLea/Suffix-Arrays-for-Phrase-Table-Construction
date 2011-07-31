cbf09ccf186ea061ba1f0e8af44f132b5cf4aab2
Lea Frermann <l1@frermann.de>
2011-07-31 14:47:59 +0200

    Longest Common Prefix method for SuffixArray

--- suffixarrays.go
+++ suffixarrays.go
@@ -7,5 +7,7 @@ import "textprocessing"
 func main() {
 	input := textprocessing.NewText("a b r a c a d a b r a")
 	suffixArray := textprocessing.NewSuffixArray(input)
+	lcp := suffixArray.Lcp(input)
 	fmt.Println(suffixArray)
+	fmt.Println(lcp)
 }
