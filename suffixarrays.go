98341c1852b90719fabb5defe19ba393336a95f0
Lea Frermann <l1@frermann.de>
2011-08-07 17:40:10 +0200

    build sentenceIndex array (to keep possiblility of sentence alignment)

--- suffixarrays.go
+++ suffixarrays.go
@@ -2,12 +2,34 @@ package main
 
 import "fmt"
 import "textprocessing"
-
+import "io/ioutil"
+import "strings"
 
 func main() {
-	input := textprocessing.NewText("a b r a c a d a b r a")
-	suffixArray := textprocessing.NewSuffixArray(input)
-	lcp := suffixArray.Lcp(input)
+	var sentenceIndices []int
+	var sourceText *textprocessing.Text = &textprocessing.Text{}
+	{
+		rawText, _ := ioutil.ReadFile("/home/lea/corpora/test")
+		sentences := strings.Split(string(rawText), "\n", -1)
+		sentenceTexts := make([]textprocessing.Text, len(sentences))
+		corpusLength := 0
+		for idx, sentence := range sentences {
+			sentenceTexts[idx] = *textprocessing.NewText(sentence)
+			corpusLength += sentenceTexts[idx].Length()
+		}
+		sentenceIndices = make([]int, 0, corpusLength)
+		input := make([]textprocessing.Word, 0, corpusLength)
+		for idx, sentence := range sentenceTexts {
+			for ii := 0; ii < sentence.Length(); ii++ {
+				sentenceIndices = append(sentenceIndices, idx)
+			}
+			input = append(input, sentence...)
+		}
+		*sourceText = textprocessing.Text(input)
+	}
+	suffixArray := textprocessing.NewSuffixArray(sourceText)
+	lcp := suffixArray.Lcp()
 	fmt.Println(suffixArray)
 	fmt.Println(lcp)
+	fmt.Println(sentenceIndices)
 }
